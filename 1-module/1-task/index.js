791215102361df2d3bbf5d196e105994e50c6429
onfim11 <onfim11@gmail.com>
2021-12-04 19:14:33 +0300

    factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let b = n;
+  let t = n;
+  if(t == 0){
+    t = 1;
+    return t;
+  }else{
+    for(let i = b; i > 1; i--){
+    t = t * (i - 1);
+    }
+    return t;
+    }
 }
